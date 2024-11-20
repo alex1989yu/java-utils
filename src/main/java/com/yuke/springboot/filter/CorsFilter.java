42f01a171dc8ed7b469f3060d2ba4d83e4f60bd2
yuke198907 <yuke198907@126.com>
2024-11-20 18:12:40 +0800

    CorsFilter配置

--- src/main/java/com/yuke/springboot/filter/CorsFilter.java
+++ src/main/java/com/yuke/springboot/filter/CorsFilter.java
@@ -13,7 +13,19 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.addHeader("Access-Control-Allow-Credentials", "true");
+        res.addHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,PUT,DELETE,PATCH,HEAD");
+        res.setHeader("Access-Control-Allow-Max-Age", "3600");
+        res.setHeader("Access-Control-Allow-Headers", "*");
 
+        if ("OPTIONS".equals(((HttpServletRequest) request).getMethod())) {
+            res.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+        chain.doFilter(request, response);
     }
 
     @Override
