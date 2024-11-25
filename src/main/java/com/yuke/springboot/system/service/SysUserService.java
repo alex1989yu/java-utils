f12aa3be82b8bff0925baab091d902f843d07270
yuke198907 <yuke198907@126.com>
2024-11-25 15:46:24 +0800

    增删改查Demo

---
+++ src/main/java/com/yuke/springboot/system/service/SysUserService.java
@@ -0,0 +1,8 @@
+package com.yuke.springboot.system.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yuke.springboot.system.entity.SysUserInfo;
+
+public interface SysUserService extends IService<SysUserInfo> {
+    public SysUserInfo findSysUserInfoByLoginname(String loginname);
+}
---
+++ src/main/java/com/yuke/springboot/system/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.yuke.springboot.system.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yuke.springboot.system.entity.SysUserInfo;
+import com.yuke.springboot.system.mapper.SysUserMapper;
+import com.yuke.springboot.system.service.SysUserService;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUserInfo> implements SysUserService {
+
+    @Override
+    public SysUserInfo findSysUserInfoByLoginname(String loginname) {
+        if (StringUtils.isEmpty(loginname)) {
+            return null;
+        }
+        return baseMapper.findSysUserInfoByLoginname(loginname);
+    }
+}
