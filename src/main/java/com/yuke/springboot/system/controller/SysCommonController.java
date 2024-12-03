b63caa5f4050baf7671233d64c021f4ef953c703
yuke198907 <yuke198907@126.com>
2024-12-03 18:05:07 +0800

    下载文件优化

--- src/main/java/com/yuke/springboot/system/controller/SysCommonController.java
+++ src/main/java/com/yuke/springboot/system/controller/SysCommonController.java
@@ -20,6 +20,72 @@ import java.net.URLDecoder;
 @RequestMapping("/common")
 public class SysCommonController {
 
+    @GetMapping(value = "/static/**")
+    public void view(HttpServletRequest request, HttpServletResponse response) {
+        // ISO-8859-1 ==> UTF-8 进行编码转换
+        String imgPath = extractPathFromPattern(request);
+        if(ConvertUtils.isEmpty(imgPath) || imgPath=="null"){
+            return;
+        }
+        // 其余处理略
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        try {
+            imgPath = imgPath.replace("..", "");
+            if (imgPath.endsWith(",")) {
+                imgPath = imgPath.substring(0, imgPath.length() - 1);
+            }
+            String filePath = "" + File.separator + imgPath;//实际文件路径
+            File file = new File(filePath);
+            if(!file.exists()){
+                response.setStatus(404);
+                throw new RuntimeException("文件不存在..");
+            }
+            response.setContentType("application/force-download");// 设置强制下载不打开
+            response.addHeader("Content-Disposition", "attachment;fileName=" + new String(file.getName().getBytes("UTF-8"),"iso-8859-1"));
+            inputStream = new BufferedInputStream(new FileInputStream(filePath));
+            outputStream = response.getOutputStream();
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = inputStream.read(buf)) > 0) {
+                outputStream.write(buf, 0, len);
+            }
+            response.flushBuffer();
+        } catch (IOException e) {
+            log.error("预览文件失败" + e.getMessage());
+            response.setStatus(404);
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    log.error(e.getMessage(), e);
+                }
+            }
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    log.error(e.getMessage(), e);
+                }
+            }
+        }
+
+    }
+
+    /**
+     *  把指定URL后的字符串全部截断当成参数
+     *  这么做是为了防止URL中包含中文或者特殊字符（/等）时，匹配不了的问题
+     * @param request
+     * @return
+     */
+    private static String extractPathFromPattern(final HttpServletRequest request) {
+        String path = (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+        String bestMatchPattern = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        return new AntPathMatcher().extractPathWithinPattern(bestMatchPattern, path);
+    }
+
     @RequestMapping("/downloadUrl")
     public void downloadUrl(@RequestParam("url") String url, HttpServletResponse response) {
         InputStream inputStream = null;
