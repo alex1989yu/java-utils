e5c30b31b5a37012f19ab2550080af35a0b28a10
yuke198907 <yuke198907@126.com>
2024-12-09 09:33:48 +0800

    servlet兼容

---
+++ src/main/java/com/yuke/springboot/servlet/DeptUserServlet.java
@@ -0,0 +1,29 @@
+package com.yuke.springboot.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@WebServlet(urlPatterns = "/deptUser.do")
+public class DeptUserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("doGet...");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //request.setCharacterEncoding("utf-8");
+        log.info("doPost...");
+        /*PrintWriter out = response.getWriter();
+        out.println("ok");
+        out.close();*/
+    }
+}
