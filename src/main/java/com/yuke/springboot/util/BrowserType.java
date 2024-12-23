3971f17b2002807bf2d43e5a0a98a516b13123a9
yuke198907 <yuke198907@126.com>
2024-12-23 13:12:31 +0800

    获取浏览器版本

---
+++ src/main/java/com/yuke/springboot/util/BrowserType.java
@@ -0,0 +1,11 @@
+package com.yuke.springboot.util;
+
+/**
+ * 浏览类型
+ */
+public enum BrowserType {
+    /**
+     * 浏览类型 IE11,IE10,IE9,IE8,IE7,IE6,Firefox,Safari,Chrome,Opera,Camino,Gecko
+     */
+	IE11,IE10,IE9,IE8,IE7,IE6,Firefox,Safari,Chrome,Opera,Camino,Gecko
+}
