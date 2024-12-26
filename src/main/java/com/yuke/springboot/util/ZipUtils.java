2fdda6a459ed18e70e705bef6c6f82510213efa2
yuke198907 <yuke198907@126.com>
2024-12-26 16:04:58 +0800

    ZIP压缩文件操作工具类

---
+++ src/main/java/com/yuke/springboot/util/ZipUtils.java
@@ -0,0 +1,374 @@
+package com.yuke.springboot.util;
+
+
+import lombok.extern.slf4j.Slf4j;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.FileHeader;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.util.Zip4jConstants;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * ZIP压缩文件操作工具类、支持密码
+ */
+@Slf4j
+public final class ZipUtils {
+
+    /**
+     * 默认私有构造器
+     */
+    private ZipUtils() {
+
+    }
+
+    /**
+     * 使用给定密码解压指定的ZIP压缩文件到指定目录
+     * <p>
+     * 如果指定目录不存在,可以自动创建,不合法的路径将导致异常被抛出
+     * @param zip 指定的ZIP压缩文件
+     * @param dest 解压目录
+     * @param passwd ZIP文件的密码
+     * @return 解压后文件数组
+     * @throws ZipException 压缩文件有损坏或者解压缩失败抛出
+     */
+    public static File [] unzip(String zip, String dest, String passwd) throws ZipException {
+        File zipFile = new File(zip);
+        return unzip(zipFile, dest, passwd);
+    }
+
+    /**
+     * 使用给定密码解压指定的ZIP压缩文件到当前目录
+     * @param zip 指定的ZIP压缩文件
+     * @param passwd ZIP文件的密码
+     * @return  解压后文件数组
+     * @throws ZipException 压缩文件有损坏或者解压缩失败抛出
+     */
+    public static File [] unzip(String zip, String passwd) throws ZipException {
+        File zipFile = new File(zip);
+        File parentDir = zipFile.getParentFile();
+        return unzip(zipFile, parentDir.getAbsolutePath(), passwd);
+    }
+
+    /**
+     * 使用给定密码解压指定的ZIP压缩文件到指定目录
+     * <p>
+     * 如果指定目录不存在,可以自动创建,不合法的路径将导致异常被抛出
+     * @param zipFile 指定的ZIP压缩文件
+     * @param dest 解压目录
+     * @param passwd ZIP文件的密码
+     * @return  解压后文件数组
+     * @throws ZipException 压缩文件有损坏或者解压缩失败抛出
+     */
+    public static File [] unzip(File zipFile, String dest, String passwd) throws ZipException {
+        ZipFile zFile = new ZipFile(zipFile);
+        zFile.setFileNameCharset("UTF-8");
+        if (!zFile.isValidZipFile()) {
+            throw new ZipException("压缩文件不合法,可能被损坏.");
+        }
+        File destDir = new File(dest);
+        if (destDir.isDirectory() && !destDir.exists()) {
+            destDir.mkdir();
+        }
+        if (zFile.isEncrypted()) {
+            zFile.setPassword(passwd.toCharArray());
+        }
+        zFile.extractAll(dest);
+        @SuppressWarnings("unchecked")
+        List<FileHeader> headerList = zFile.getFileHeaders();
+        List<File> extractedFileList = new ArrayList<>();
+        for(FileHeader fileHeader : headerList) {
+            if (!fileHeader.isDirectory()) {
+                extractedFileList.add(new File(destDir,fileHeader.getFileName()));
+            }
+        }
+        File [] extractedFiles = new File[extractedFileList.size()];
+        extractedFileList.toArray(extractedFiles);
+        return extractedFiles;
+    }
+
+    /**
+     * 压缩指定文件到当前文件夹
+     * @param src 要压缩的指定文件
+     * @return 最终的压缩文件存放的绝对路径,如果为null则说明压缩失败.
+     */
+    public static String zip(String src) {
+        return zip(src,null);
+    }
+
+    /**
+     * 使用给定密码压缩指定文件或文件夹到当前目录
+     * @param src 要压缩的文件
+     * @param passwd 压缩使用的密码
+     * @return 最终的压缩文件存放的绝对路径,如果为null则说明压缩失败.
+     */
+    public static String zip(String src, String passwd) {
+        return zip(src, null, passwd);
+    }
+
+    /**
+     * 使用给定密码压缩指定文件或文件夹到当前目录
+     * @param src 要压缩的文件
+     * @param dest 压缩文件存放路径
+     * @param passwd 压缩使用的密码
+     * @return 最终的压缩文件存放的绝对路径,如果为null则说明压缩失败.
+     */
+    public static String zip(String src, String dest, String passwd) {
+        return zip(src, dest, true, passwd);
+    }
+
+    /**
+     * 使用给定密码压缩指定文件或文件夹到指定位置.
+     * <p>
+     * dest可传最终压缩文件存放的绝对路径,也可以传存放目录,也可以传null或者"".<br />
+     * 如果传null或者""则将压缩文件存放在当前目录,即跟源文件同目录,压缩文件名取源文件名,以.zip为后缀;<br />
+     * 如果以路径分隔符(File.separator)结尾,则视为目录,压缩文件名取源文件名,以.zip为后缀,否则视为文件名.
+     * @param src 要压缩的文件或文件夹路径
+     * @param dest 压缩文件存放路径
+     * @param isCreateDir 是否在压缩文件里创建目录,仅在压缩文件为目录时有效.<br />
+     * 如果为false,将直接压缩目录下文件到压缩文件.
+     * @param passwd 压缩使用的密码
+     * @return 最终的压缩文件存放的绝对路径,如果为null则说明压缩失败.
+     */
+    public static String zip(String src, String dest, boolean isCreateDir, String passwd) {
+        File srcFile = new File(src);
+        dest = buildDestinationZipFilePath(srcFile, dest);
+        ZipParameters parameters = new ZipParameters();
+        parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);           // 压缩方式
+        parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);    // 压缩级别
+        if (!StringUtils.isEmpty(passwd)) {
+            parameters.setEncryptFiles(true);
+            parameters.setEncryptionMethod(Zip4jConstants.ENC_METHOD_STANDARD); // 加密方式
+            parameters.setPassword(passwd.toCharArray());
+        }
+        try {
+            ZipFile zipFile = new ZipFile(dest);
+            if (srcFile.isDirectory()) {
+                // 如果不创建目录的话,将直接把给定目录下的文件压缩到压缩文件,即没有目录结构
+                if (!isCreateDir) {
+                    File [] subFiles = srcFile.listFiles();
+                    ArrayList<File> temp = new ArrayList<>();
+                    Collections.addAll(temp, subFiles);
+                    zipFile.addFiles(temp, parameters);
+                    return dest;
+                }
+                zipFile.addFolder(srcFile, parameters);
+            } else {
+                zipFile.addFile(srcFile, parameters);
+            }
+            return dest;
+        } catch (ZipException e) {
+            log.error("解压失败", e);
+        }
+        return null;
+    }
+
+    /**
+     * 构建压缩文件存放路径,如果不存在将会创建
+     * 传入的可能是文件名或者目录,也可能不传,此方法用以转换最终压缩文件的存放路径
+     * @param srcFile 源文件
+     * @param destParam 压缩目标路径
+     * @return 正确的压缩文件存放路径
+     */
+    private static String buildDestinationZipFilePath(File srcFile,String destParam) {
+        if (StringUtils.isEmpty(destParam)) {
+            if (srcFile.isDirectory()) {
+                destParam = new StringBuilder(srcFile.getParent()).append(File.separator).append(srcFile.getName())
+                        .append(".zip").toString();
+            } else {
+                String fileName = srcFile.getName().substring(0, srcFile.getName().lastIndexOf("."));
+                destParam = srcFile.getParent() + File.separator + fileName + ".zip";
+            }
+        } else {
+            createDestDirectoryIfNecessary(destParam);  // 在指定路径不存在的情况下将其创建出来
+            if (destParam.endsWith(File.separator)) {
+                String fileName = "";
+                if (srcFile.isDirectory()) {
+                    fileName = srcFile.getName();
+                } else {
+                    fileName = srcFile.getName().substring(0, srcFile.getName().lastIndexOf("."));
+                }
+                destParam = new StringBuilder(destParam).append(fileName).append(".zip").toString();
+            }
+        }
+        return destParam;
+    }
+
+    /**
+     * 在必要的情况下创建压缩文件存放目录,比如指定的存放路径并没有被创建
+     * @param destParam 指定的存放路径,有可能该路径并没有被创建
+     */
+    private static void createDestDirectoryIfNecessary(String destParam) {
+        File destDir = null;
+        if (destParam.endsWith(File.separator)) {
+            destDir = new File(destParam);
+        } else {
+            destDir = new File(destParam.substring(0, destParam.lastIndexOf(File.separator)));
+        }
+        if (!destDir.exists()) {
+            destDir.mkdirs();
+        }
+    }
+
+    /**
+     * zip压缩功能,压缩sourcePath(文件夹目录)下所有文件，包括子目录
+     * @param sourcePath 待压缩目录;
+     * @param zipFile 压缩文件
+     */
+    public static void zipFiles(String sourcePath, File zipFile) {
+        List<File> fileList = getSubFiles(new File(sourcePath)); //得到待压缩的文件夹的所有内容
+        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFile))) {
+            ZipEntry ze = null;
+            byte[] buf = new byte[1024];
+            int readLen = 0;
+            for (int i = 0; i < fileList.size(); i++) { //遍历要压缩的所有子文件
+                File file = fileList.get(i);
+                ze = new ZipEntry(getAbsFileName(sourcePath, file));
+                ze.setSize(file.length());
+                ze.setTime(file.lastModified());
+                zos.putNextEntry(ze);
+                try (InputStream is = new BufferedInputStream(new FileInputStream(file))) {
+                    while ((readLen = is.read(buf, 0, 1024)) != -1) {
+                        zos.write(buf, 0, readLen);
+                    }
+                } catch (IOException e) {
+                    log.error("文件流读取失败！", e);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            log.error("目标文件不存在！", e);
+        } catch (IOException e) {
+            log.error("文件读取失败！", e);
+        }
+    }
+
+    /**
+     * 取得指定目录下的所有文件列表，包括子目录.
+     * @param baseDir File 指定的目录
+     * @return 包含java.io.File的List
+     */
+    private static List<File> getSubFiles(File baseDir) {
+        List<File> ret = new ArrayList<File>();
+        File[] tmp = baseDir.listFiles();
+        if (null == tmp) {
+            return ret;
+        }
+        for (int i = 0; i < tmp.length; i++) {
+            if (tmp[i].isFile())
+                ret.add(tmp[i]);
+            if (tmp[i].isDirectory())
+                ret.addAll(getSubFiles(tmp[i]));
+        }
+        return ret;
+    }
+
+    /**
+     * 给定根目录，返回另一个文件名的相对路径，用于zip文件中的路径.
+     * @param baseDir java.lang.String 根目录
+     * @param realFileName java.io.File 实际的文件名
+     * @return 相对文件名
+     */
+    private static String getAbsFileName(String baseDir, File realFileName) {
+        File real = realFileName;
+        File base = new File(baseDir);
+        String ret = real.getName();
+        while (true) {
+            real = real.getParentFile();
+            if (real == null)
+                break;
+            if (real.equals(base))
+                break;
+            else
+                ret = real.getName() + "/" + ret;
+        }
+        return ret;
+    }
+    /**
+     * 压缩成ZIP 方法1
+     * @param srcDir 被压缩文件夹路径
+     * @param fileName 压好放哪
+     * @throws RuntimeException 压缩失败会抛出运行时异常
+     */
+    public static void toZip(String srcDir, String fileName, boolean keepFile) {
+        try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(fileName));
+                ZipOutputStream zos = new ZipOutputStream(bos);) {
+            File sourceFile = new File(srcDir);
+            compress(sourceFile, zos, sourceFile.getName(), keepFile);
+        } catch (Exception e) {
+            log.error("zip error from ZipUtils", e);
+        }
+
+    }
+
+    /**
+     * 递归压缩方法
+     * @param sourceFile 源文件
+     * @param zos        zip输出流
+     * @param name       压缩后的名称
+     * @param KeepDirStructure  是否保留原来的目录结构,true:保留目录结构;
+     *                          false:所有文件跑到压缩包根目录下(注意：不保留目录结构可能会出现同名文件,会压缩失败)
+     * @throws Exception
+     */
+    private static void compress(File sourceFile, ZipOutputStream zos, String name, boolean KeepDirStructure) throws Exception {
+        byte[] buf = new byte[1024];
+        if (sourceFile.isFile()) {
+            // 向zip输出流中添加一个zip实体，构造器中name为zip实体的文件的名字
+            zos.putNextEntry(new ZipEntry(name));
+            // copy文件到zip输出流中
+            int len;
+            try (FileInputStream in = new FileInputStream(sourceFile);) {
+                while ((len = in.read(buf)) != -1) {
+                    zos.write(buf, 0, len);
+                    zos.flush();
+                }
+                zos.closeEntry();
+            }
+        } else {
+            File[] listFiles = sourceFile.listFiles();
+            if (listFiles == null || listFiles.length == 0) {
+                // 需要保留原来的文件结构时,需要对空文件夹进行处理
+                if (KeepDirStructure) {
+                    // 空文件夹的处理
+                    zos.putNextEntry(new ZipEntry(name + "/"));
+                    // 没有文件，不需要文件的copy
+                    zos.closeEntry();
+                }
+
+            } else {
+                for (File file : listFiles) {
+                    // 判断是否需要保留原来的文件结构
+                    if (KeepDirStructure) {
+                        // 注意：file.getName()前面需要带上父文件夹的名字加一斜杠,
+                        // 不然最后压缩包中就不能保留原来的文件结构,即：所有文件都跑到压缩包根目录下了
+                        compress(file, zos, name + "/" + file.getName(), KeepDirStructure);
+                    } else {
+                        compress(file, zos, file.getName(), KeepDirStructure);
+                    }
+
+                }
+            }
+        }
+    }
+
+    /**
+     * 删除所有临时文件
+     * @param gcxtDownloadPath
+     */
+    public static void delFolder(String gcxtDownloadPath) {
+        File gcxtDownload = new File(gcxtDownloadPath);
+        try {
+            FileUtils.deleteDirectory(gcxtDownload);
+        } catch (IOException e) {
+            log.error("删除临时文件出错", e);
+        }
+
+    }
+}
