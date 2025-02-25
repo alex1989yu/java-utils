245161f36e3c76a319671a665d68fa59eb2e3c6a
yuke198907 <yuke198907@126.com>
2025-02-25 21:01:52 +0800

    判断文件名是否带盘符

--- src/main/java/com/yuke/springboot/util/CommonUtils.java
+++ src/main/java/com/yuke/springboot/util/CommonUtils.java
@@ -27,6 +27,34 @@ public class CommonUtils {
     private static String FILE_NAME_REGEX = "[^A-Za-z\\.\\(\\)\\-（）\\_0-9\\u4e00-\\u9fa5]";
 
     /**
+     * 判断文件名是否带盘符，重新处理
+     * @param fileName
+     * @return
+     */
+    public static String getFileName(String fileName){
+        //判断是否带有盘符信息
+        // Check for Unix-style path
+        int unixSep = fileName.lastIndexOf('/');
+        // Check for Windows-style path
+        int winSep = fileName.lastIndexOf('\\');
+        // Cut off at latest possible point
+        int pos = (winSep > unixSep ? winSep : unixSep);
+        if (pos != -1)  {
+            // Any sort of path separator found...
+            fileName = fileName.substring(pos + 1);
+        }
+        //替换上传文件名字的特殊字符
+        fileName = fileName.replace("=","").replace(",","").replace("&","")
+                .replace("#", "").replace("“", "").replace("”", "");
+        //替换上传文件名字中的空格
+        fileName=fileName.replaceAll("\\s","");
+        //update-beign-author:taoyan date:20220302 for: /issues/3381 online 在线表单 使用文件组件时，上传文件名中含%，下载异常
+        fileName = fileName.replaceAll(FILE_NAME_REGEX, "");
+        //update-end-author:taoyan date:20220302 for: /issues/3381 online 在线表单 使用文件组件时，上传文件名中含%，下载异常
+        return fileName;
+    }
+
+    /**
      * java 判断字符串里是否包含中文字符
      * @param str
      * @return
